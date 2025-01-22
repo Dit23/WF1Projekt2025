27fa24f6dfda096b833ccd8f01fa50ea2ec8efb4
Leon_lp9 <leon.marcel.rabe@gmail.com>
2025-01-22 13:25:48 +0100

    create and login account

--- api.php
+++ api.php
@@ -17,9 +17,56 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     switch ($method){
         case 'login':
+            if (isset($_POST['username']) === false || isset($_POST['password']) === false) {
+                echo json_encode(['success' => false, 'requestedAt' => date('Y-m-d H:i:s'), 'message' => 'Benutzername oder Passwort nicht gefunden']);
+                die();
+            }
+
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            $statement = $pdo->prepare('SELECT * FROM klassen WHERE name = :name');
+            $statement->bindParam(':name', $username);
+            $statement->execute();
+
+            $user = $statement->fetch();
+
+            if ($user === false) {
+                echo json_encode(['success' => false, 'requestedAt' => date('Y-m-d H:i:s'), 'message' => 'Benutzer nicht gefunden']);
+                die();
+            }
+
+            if (password_verify($password, $user['hashPasswort']) === false) {
+                echo json_encode(['success' => false, 'requestedAt' => date('Y-m-d H:i:s'), 'message' => 'Passwort falsch']);
+                die();
+            }
+
+            $_SESSION['userId'] = $user['id'];
+            $_SESSION['name'] = $user['name'];
+            $_SESSION['admin'] = $user['admin'];
+
+            echo json_encode(['success' => true, 'requestedAt' => date('Y-m-d H:i:s'), 'message' => 'Login erfolgreich']);
             break;
-        case 'register':
+
+        case 'createNewClassAccount':
+            if (isset($_POST['username']) === false || isset($_POST['password']) === false) {
+                echo json_encode(['success' => false, 'requestedAt' => date('Y-m-d H:i:s'), 'message' => 'Benutzername oder Passwort nicht gefunden']);
+                die();
+            }
+
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            $hashPassword = password_hash($password, PASSWORD_DEFAULT);
+
+            $statement = $pdo->prepare('INSERT INTO klassen (name, hashPasswort) VALUES (:name, :hashPasswort)');
+            $statement->bindParam(':name', $username);
+            $statement->bindParam(':hashPasswort', $hashPassword);
+            $statement->execute();
+
+            echo json_encode(['success' => true, 'requestedAt' => date('Y-m-d H:i:s'), 'message' => 'Benutzer erfolgreich erstellt']);
             break;
+
         case 'logout':
             session_destroy();
             echo json_encode(['success' => true, 'requestedAt' => date('Y-m-d H:i:s'), 'message' => 'Logout erfolgreich']);
