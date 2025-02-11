d12f5911b88edea6fd4815e48c6889e61ffefd31
MrsFreckles <luna.kreyenberg@gmail.com>
2025-02-11 13:14:51 +0100

    feat: add stylesheet link and update footer content for improved branding

--- shared/footer.php
+++ shared/footer.php
@@ -7,7 +7,7 @@
             <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
                 <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
             </a>
-            <p class="text-body-secondary">© 2024</p>
+            <p class="text-body-secondary">Digitaler Schulplaner</p>
         </div>
 
         <div class="col mb-3">
@@ -17,34 +17,43 @@
         <div class="col mb-3">
             <h5>BK Ostvest</h5>
             <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="https://www.google.de/maps/place/Hans-B%C3%B6ckler-Stra%C3%9Fe+2,+45711+Datteln/@51.6497671,7.3316273,17z/data=!4m6!3m5!1s0x47b9039968757bb1:0x566b25d2fb87650c!8m2!3d51.6497671!4d7.3342022!16s%2Fg%2F11vbjc6f18?entry=ttu&g_ep=EgoyMDI1MDEyMi4wIKXMDSoASAFQAw%3D%3D" class="nav-link p-0 text-body-secondary">Hans-Böcker-Straße 2</a></li>
-                <li class="nav-item mb-2"><a href="tel:023633780" class="nav-link p-0 text-body-secondary">02363 378-0</a></li>
-                <li class="nav-item mb-2"><a href="mailto:mail@bk-ostvest.de" class="nav-link p-0 text-body-secondary">mail@bk-ostvest.de</a></li>
-                <li class="nav-item mb-2"><a href="https://www.facebook.com/BKOstvest?sk=wall" class="nav-link p-0 text-body-secondary">Facebook</a></li>
-                <li class="nav-item mb-2"><a href="https://www.instagram.com/berufskolleg_ostvest/" class="nav-link p-0 text-body-secondary">Instagram</a></li>
+                <li class="footer-element" class="nav-item mb-2"><a target="_blank" rel="noopener noreferrer" href="https://www.google.de/maps/place/Hans-B%C3%B6ckler-Stra%C3%9Fe+2,+45711+Datteln/@51.6497671,7.3316273,17z/data=!4m6!3m5!1s0x47b9039968757bb1:0x566b25d2fb87650c!8m2!3d51.6497671!4d7.3342022!16s%2Fg%2F11vbjc6f18?entry=ttu&g_ep=EgoyMDI1MDEyMi4wIKXMDSoASAFQAw%3D%3D" class="nav-link p-0 text-body-secondary">Hans-Böcker-Straße 2</a></li>
+                <li class="footer-element" class="nav-item mb-2"><a target="_blank" rel="noopener noreferrer" href="tel:023633780" class="nav-link p-0 text-body-secondary">02363 378-0</a></li>
+                <li class="footer-element" class="nav-item mb-2"><a target="_blank" rel="noopener noreferrer" href="mailto:mail@bk-ostvest.de" class="nav-link p-0 text-body-secondary">mail@bk-ostvest.de</a></li>
+                <li class="footer-element" class="nav-item mb-2"><a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/BKOstvest?sk=wall" class="nav-link p-0 text-body-secondary">Facebook</a></li>
+                <li class="footer-element" class="nav-item mb-2"><a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/berufskolleg_ostvest/" class="nav-link p-0 text-body-secondary">Instagram</a></li>
             </ul>
         </div>
 
         <div class="col mb-3">
             <h5>intern systems</h5>
             <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="https://arche.webuntis.com/WebUntis/?school=BK-Ostvest#/basic/login" class="nav-link p-0 text-body-secondary">Untis</a></li>
-                <li class="nav-item mb-2"><a href="https://bkodatteln.de/iserv/auth/login?_target_path=/iserv/auth/auth?_iserv_app_url%3D%2Fiserv%2Fapp%2Flogin%253Ftarget%253D%25252Fiserv%25252F%26client_id%3D15_3k9t9fsr6e0w8w0go4cwckgo8kg0ko8s8gw4c800ggoocccc04%26nonce%3Dde178b21-0e23-4e48-ae8b-a6173b431a86%26redirect_uri%3Dhttps%253A%2F%2Fbkodatteln.de%2Fiserv%2Fapp%2Fauthentication%2Fredirect%26response_type%3Dcode%26scope%3Dopenid%2520uuid%2520iserv%253Asession-id%2520iserv%253Aweb-ui%2520iserv%253A2fa%253Aconfiguration%2520iserv%253Aaccess-groups%26state%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjEifQ.eyJyZWRpcmVjdF91cmkiOiJodHRwczpcL1wvYmtvZGF0dGVsbi5kZVwvaXNlcnYiLCJub25jZSI6ImRlMTc4YjIxLTBlMjMtNGU0OC1hZThiLWE2MTczYjQzMWE4NiIsImFkbWluIjpmYWxzZSwiaXNzIjoiaHR0cHM6XC9cL2Jrb2RhdHRlbG4uZGVcL2lzZXJ2XC8iLCJleHAiOjE3NDU4MjY2NDMsIm5iZiI6MTczODA1MDU4MywiaWF0IjoxNzM4MDUwNjQzLCJzaWQiOiIifQ.uWH7vq6XPKHTa--X78odesxWcpfpHBNEhrfTtY7Fwjp_GfWhwNR_qenMqOIjfDqrHWqZClXMUzzW0frYa574ww" class="nav-link p-0 text-body-secondary">IServe</a></li>
-                <li class="nav-item mb-2"><a href="https://bk-ostvest.lms.schulon.org/login/index.php" class="nav-link p-0 text-body-secondary">moodle</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+                <li class="footer-element" class="nav-item mb-2"><a target="_blank" rel="noopener noreferrer" href="https://arche.webuntis.com/WebUntis/?school=BK-Ostvest#/basic/login" class="nav-link p-0 text-body-secondary">Untis</a></li>
+                <li class="footer-element" class="nav-item mb-2"><a target="_blank" rel="noopener noreferrer" href="https://bkodatteln.de/iserv/auth/login?_target_path=/iserv/auth/auth?_iserv_app_url%3D%2Fiserv%2Fapp%2Flogin%253Ftarget%253D%25252Fiserv%25252F%26client_id%3D15_3k9t9fsr6e0w8w0go4cwckgo8kg0ko8s8gw4c800ggoocccc04%26nonce%3Dde178b21-0e23-4e48-ae8b-a6173b431a86%26redirect_uri%3Dhttps%253A%2F%2Fbkodatteln.de%2Fiserv%2Fapp%2Fauthentication%2Fredirect%26response_type%3Dcode%26scope%3Dopenid%2520uuid%2520iserv%253Asession-id%2520iserv%253Aweb-ui%2520iserv%253A2fa%253Aconfiguration%2520iserv%253Aaccess-groups%26state%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjEifQ.eyJyZWRpcmVjdF91cmkiOiJodHRwczpcL1wvYmtvZGF0dGVsbi5kZVwvaXNlcnYiLCJub25jZSI6ImRlMTc4YjIxLTBlMjMtNGU0OC1hZThiLWE2MTczYjQzMWE4NiIsImFkbWluIjpmYWxzZSwiaXNzIjoiaHR0cHM6XC9cL2Jrb2RhdHRlbG4uZGVcL2lzZXJ2XC8iLCJleHAiOjE3NDU4MjY2NDMsIm5iZiI6MTczODA1MDU4MywiaWF0IjoxNzM4MDUwNjQzLCJzaWQiOiIifQ.uWH7vq6XPKHTa--X78odesxWcpfpHBNEhrfTtY7Fwjp_GfWhwNR_qenMqOIjfDqrHWqZClXMUzzW0frYa574ww" class="nav-link p-0 text-body-secondary">IServ</a></li>
+                <li class="footer-element" class="nav-item mb-2"><a target="_blank" rel="noopener noreferrer" href="https://bk-ostvest.lms.schulon.org/login/index.php" class="nav-link p-0 text-body-secondary">Moodle</a></li>
+                <!--<li class="footer-element" class="nav-item mb-2"><a target="_blank" rel="noopener noreferrer" href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>-->
+                <li class="footer-element" class="nav-item mb-2"><a target="_blank" rel="noopener noreferrer" href="#" class="nav-link p-0 text-body-secondary">About</a></li>
             </ul>
         </div>
 
-        <div class="col mb-3">
+        <!--<div class="col mb-3">
             <h5>Section</h5>
             <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+                <li class="footer-element" class="nav-item mb-2"><a target="_blank" rel="noopener noreferrer" href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                <li class="footer-element"  class="nav-item mb-2"><a target="_blank" rel="noopener noreferrer" href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+                <li class="footer-element"  class="nav-item mb-2"><a target="_blank" rel="noopener noreferrer" href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+                <li class="footer-element"  class="nav-item mb-2"><a target="_blank" rel="noopener noreferrer" href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+                <li class="footer-element"  class="nav-item mb-2"><a target="_blank" rel="noopener noreferrer" href="#" class="nav-link p-0 text-body-secondary">About</a></li>
             </ul>
-        </div>
+        </div>-->
     </footer>
+
+    <style>
+        .footer-element {
+            margin-bottom: 1em;
+        }
+        .footer-element:hover {
+            font-weight: bold;
+        }
+    </style>
 </div>
